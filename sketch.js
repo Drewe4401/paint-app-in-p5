8a06eb8f4813f76e3c32b1ba172e8af2d937fd90
Andrew Ellender <70233363+Drewe4401@users.noreply.github.com>
2023-04-01 17:24:39 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,49 @@
 var brush;
+
 function setup() {
   createCanvas(2600, 1900);
   brush = color(50);
 }
 
 function draw() {
-if(mouseIsPressed){
-  if(mouseX <= 22){
-    if(mouseY <= 20){
-      brush = color(255,0,0);
-    }else if (mouseY <= 40){
-      brush = color(255,165,0);
-    }else if(mouseY <= 60){
-      brush = color(255,255,0);
-    }else if(mouseY <= 80){
-      brush = color(144,238,144);
-    }else if(mouseY <= 100){
-      brush = color(0, 204,255);
-    }else if(mouseY <= 120){
-      brush = color(0,0,255);
-    }else if(mouseY <= 140){
-      brush = color(255, 0, 255);
-    }else if(mouseY <= 160){
-      brush = color(101, 67, 33);
-    }else if(mouseY <= 180){
-      brush = color(255);
-    }else if(mouseY <= 200){
-      brush = color(0);
+  if (mouseIsPressed) {
+    if (mouseX <= 22) {
+      if (mouseY <= 20) {
+        brush = color(255, 0, 0);
+      } else if (mouseY <= 40) {
+        brush = color(255, 165, 0);
+      } else if (mouseY <= 60) {
+        brush = color(255, 255, 0);
+      } else if (mouseY <= 80) {
+        brush = color(144, 238, 144);
+      } else if (mouseY <= 100) {
+        brush = color(0, 204, 255);
+      } else if (mouseY <= 120) {
+        brush = color(0, 0, 255);
+      } else if (mouseY <= 140) {
+        brush = color(255, 0, 255);
+      } else if (mouseY <= 160) {
+        brush = color(101, 67, 33);
+      } else if (mouseY <= 180) {
+        brush = color(255);
+      } else if (mouseY <= 200) {
+        brush = color(0);
+      } else if (mouseY <= 220) {
+        background(255);
+      }
     }
+    stroke(brush);
+    strokeWeight(20);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
-  stroke(brush);
-  strokeWeight(20);
-  line(mouseX, mouseY, pmouseX, pmouseY);
-}
 
-
-
-
-strokeWeight(1);
+  strokeWeight(1);
   stroke(255);
-  fill(255,0,0); //red
+  fill(255, 0, 0); //red
   rect(2, 0, 20, 20);
   fill(255, 165, 0); //orange
   rect(2, 20, 20, 20);
-  fill(255,255,0); //yellow
+  fill(255, 255, 0); //yellow
   rect(2, 40, 20, 20);
   fill(144, 238, 144); //green
   rect(2, 60, 20, 20);
@@ -60,7 +60,10 @@ strokeWeight(1);
   fill(0); //black
   rect(2, 180, 20, 20);
 
-
+  // Erase button
+  fill(200);
+  rect(2, 220, 20, 20);
+  fill(0);
+  textSize(12);
+  text('E', 6, 234);
 }
-
-
